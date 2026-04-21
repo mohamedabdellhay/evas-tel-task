ea641d8d6735be94d4aab42c024b5c0b8abedfdd
Mohamed Abdellhay <mohamedabdellhay1@gmail.com>
2026-04-21 21:21:49 +0200

    feat(interceptor): add TransformInterceptor for standardized HTTP responses

---
+++ src/common/interceptors/transform/transform.interceptor.ts
@@ -0,0 +1,23 @@
+import {
+  Injectable,
+  NestInterceptor,
+  ExecutionContext,
+  CallHandler,
+} from '@nestjs/common';
+import { Observable, map } from 'rxjs';
+
+@Injectable()
+export class TransformInterceptor<T> implements NestInterceptor<T, any> {
+  intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+    const ctx = context.switchToHttp();
+    const response = ctx.getResponse();
+
+    return next.handle().pipe(
+      map((data) => ({
+        success: response.statusCode < 400,
+        statusCode: response.statusCode,
+        data: data
+      })),
+    );
+  }
+}
